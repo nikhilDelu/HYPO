077335a75c2df1428aa91904945b930b772ed4df
Yash Bishnoi <112739888+nikhilDelu@users.noreply.github.com>
2025-07-08 23:12:32 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -90,7 +90,7 @@ The JSON should look like this:
 
   try {
     const response = await axios.post("http://localhost:11434/api/generate", {
-      model: "gemma3",
+      model: "gemma3:1b",
       prompt,
       stream: false,
     });
