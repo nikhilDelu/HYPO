cce817e6bec6063257305dbc860ece71871a5d76
nikhilDelu <yashbishnoi2102@gmail.com>
2025-07-11 14:52:49 +0530

    polished dashboard

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -13,33 +13,60 @@ export default function RoomPage() {
   return (
     <main className="flex flex-col min-h-screen w-screen overflow-hidden">
       <div className="flex-grow-[1] flex justify-between border-b-2 border-black">
-        <span className="w-[40%] flex items-center justify-center bg-black text-white text-4xl">
+        <span className="w-[40%] flex items-center justify-center bg-black text-white text-4xl font-bold">
           HYPO
         </span>
         <div className="p-0 w-full flex justify-between border-black border-x-2 ">
-          <span className="border-r-2 w-full h-full flex items-center justify-center border-black">
+          <span className="border-r-2 w-full h-full flex items-center justify-center border-black hover:text-[#d4dad3] hover:bg-black transition-all duration-150 cursor-pointer">
             Rule 1
           </span>
-          <span className="border-r-2 w-full h-full flex items-center justify-center border-black">
+          <span className="border-r-2 w-full h-full flex items-center justify-center border-black hover:text-[#d4dad3] hover:bg-black transition-all duration-150 cursor-pointer">
             Rule 2
           </span>
-          <span className="border-r-2 w-full h-full flex items-center justify-center border-black">
+          <span className="border-r-2 w-full h-full flex items-center justify-center border-black hover:text-[#d4dad3] hover:bg-black transition-all duration-150 cursor-pointer">
             Rule 3
           </span>
-          <span className="border-r-2 w-full h-full flex items-center justify-center border-black">
+          <span className="border-r-2 w-full h-full flex items-center justify-center border-black hover:text-[#d4dad3] hover:bg-black transition-all duration-150 cursor-pointer">
             Rule 4
           </span>
-          <span className=" border-black w-full h-full flex items-center justify-center">
+          <span className=" border-black w-full h-full flex items-center justify-center hover:text-[#d4dad3] hover:bg-black transition-all duration-150 cursor-pointer">
             Rule 5
           </span>
         </div>
-        <span className="w-[20%] flex items-center justify-center text-2xl">
+        <span className="w-[20%] cursor-pointer flex items-center justify-center text-2xl">
           Enter
         </span>
       </div>
       <div className="flex-grow-[5] flex justify-between">
         <div className="w-full flex-grow[1] flex items-center justify-center p-2">
-          <div className="border-1 border-black h-32 w-52"></div>
+          <div className="cursor-pointer hover:scale-105 transition-all duration-150 border-1 border-gray-500 h-32 w-[80%] min-w-40 max-w-64 relative flex flex-col p-2 gap-1">
+            <div className="">
+              <div className="flex justify-between items-center ">
+                <div className="text-xs">WORLDWIDE (SX)</div>
+                <div className="text-xs font-extrabold italic">HYPO</div>
+              </div>
+              <div className="text-[4px] flex flex-col">
+                <span>DEPARTMENT 04</span>
+                <span>Ca21-01</span>
+              </div>
+            </div>
+            <div className="w-full h-full mb-8 flex flex-col">
+              <div className="flex justify-between">
+                <div className="border-l-1 border-t-1 border-black size-2"></div>
+                <div className="border-r-1 border-t-1 border-black size-2"></div>
+              </div>
+              <div className="h-full -mt-1 -mb-1 p-[2px] px-2">
+                <div className="flex flex-col leading-tight">
+                  <span className="text-[8px] font-semibold">Yash Bishnoi</span>
+                  <span className="text-[4px]">Score: 69</span>
+                </div>
+              </div>
+              <div className="flex justify-between">
+                <div className="border-l-1 border-b-1 border-black size-2"></div>
+                <div className="border-r-1 border-b-1 border-black size-2"></div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="w-full flex-grow[1] text-9xl font-bold uppercase flex items-center justify-between">
           <div>{weekdayName}.</div>
